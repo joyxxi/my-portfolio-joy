9d9fb8439a1d0e4c1932c7527d02835bdadb6fb6
joyxxi <joyx29580@gmail.com>
2024-09-05 22:27:40 -0700

    Add source code in footer

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,8 +4,11 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &2024 Zhixi Xu. <br />
+        &#169;2024 Zhixi Xu. <br />
         All rights reserved.
+        <a href="https://github.com/joyxxi/my-portfolio-joy" target="_blank">
+          <p>Sourtce Code</p>
+        </a>
       </p>
     </section>
   );
